7a101f38502183499109a31fec17294c068e371d
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2016-10-14 14:20:48 +0200

    Using the sphinx_rtd_theme theme, with a few extras
    In an extra CSS file, alignment is set to justify
    
    In an extra JS file, links that end with .ipynb will be changes
    so that the iPython notebooks will be opened with nbviewer

--- sphinxdoc/source/conf.py
+++ sphinxdoc/source/conf.py
@@ -76,7 +76,12 @@ def getVersion():
 
     raise Exception("No version number found")
 
-version = getVersion()
+try:
+    version = getVersion()
+except Exception as e:
+    print("Warning: can't get version: {}".format(e))
+    version = "unknown"
+
 # The full version, including alpha/beta/rc tags.
 release = version
 
@@ -124,14 +129,21 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'alabaster'
-html_theme = "classic-mod"
-html_theme_path = [ "." ]
+#html_theme = "classic-mod"
+# html_theme_path = [ "." ]
+
+html_theme = 'sphinx_rtd_theme'
+html_context = {
+    'extra_css_files': [ "_static/theme-mod.css" ],
+    'script_files': [ "_static/inspectlinks.js" ],
+}
 
 #html_theme_options = {
 #    "rightsidebar": "true",
@@ -331,14 +343,14 @@ finally:
 
 curpath = os.getcwd()
 try:
-    if os.getenv("READTHEDOCS") == 'True':
+    if on_rtd:
         dstdir = "_build/html"
     else:
         dstdir = "../build/html"
     
     dstdir = os.path.abspath(dstdir)
 
-    if os.getenv("READTHEDOCS") == 'True':
+    if on_rtd and os.path.exists("../../Doxyfile"):
         os.chdir("../../")
         subprocess.call("doxygen", shell=True)
         docudir = os.path.join(dstdir, "documentation")
