09205c49ff2aec77b0208810e3f0f15f4c4dd2ee
twigglits <jnaude60512@gmail.com>
2024-02-27 14:01:35 +0000

    updated tutorial.py with notes on how to add the symbolic link

--- python/tutorial.py
+++ python/tutorial.py
@@ -1,13 +1,22 @@
 import pysimpactcyan
+import sys
+
+# Check which directories are on PATH
+# echo $PATH
+# Assuming that 'usr/local/bin' is on PATH, then you need to add a symbolic link as follows:
+# ln -s /home/jupyter/who-aids-prop/build/simpact-cyan-release /usr/local/bin/simpact-cyan-release
+# you should then be able to call the package from anywhere in your home directory
+
+# Continue with the rest of your PySimpactCyan code
 simpact = pysimpactcyan.PySimpactCyan()
 
-myMatrix = [ [1, 3], [2, 4] ]
-data = { }
-
+myMatrix = [[1, 3], [2, 4]]
+data = {}
 data["csvMatrix"] = myMatrix
 
-cfg = { }
+cfg = {}
 cfg["person.geo.dist2d.type"] = "discrete"
 cfg["person.geo.dist2d.discrete.densfile"] = "data:csvMatrix"
 
-simpact.run(cfg,"/tmp/simpacttest", dataFiles=data)
+# Run PySimpactCyan
+simpact.run(cfg, "/tmp/simpacttest", dataFiles=data)
