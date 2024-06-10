15bb2346283cf465f3e9f987619ba79b74ad32e4
twigglits <jnaude60512@gmail.com>
2024-06-10 11:03:39 +0000

    updated tutorial file

--- build/python/tutorial.py
+++ build/python/tutorial.py
@@ -20,25 +20,25 @@ cfg = { "population.nummen": 1000,
         "population.numwomen": 1000,
         "population.simtime": 50,
         "population.agedistfile": "/home/jupyter/who-aids-prop/build/python/eswatini_2023.csv",
-        "periodiclogging.interval": 5,
+        "periodiclogging.interval": 1,
         "periodiclogging.starttime": 0
       }
 cfg["EventVMMC.enabled"] = "false"
 
-# cfg["EventCondom.enabled"] = "false"
+cfg["EventCondom.enabled"] = "false"
 
 iv1 = { }
-iv1["time"] = 20 #start vmmc 20years after simulation has started
+iv1["time"] = 20 #start intervention 20years after simulation has started
 iv1["EventCondom.enabled"] = "true"
 
 cfg["EventCondom.m_condomprobDist.dist.type"] = "uniform"
 cfg["EventCondom.m_condomprobDist.dist.uniform.max"] = 1
 cfg["EventCondom.m_condomprobDist.dist.uniform.min"] = 0
-cfg["EventCondom.threshold"] = 0.5      # threshold for individual 
-cfg["hivtransmission.threshold"] = 0.5  # threshold for formation
-cfg["hivtransmission.m_condomformationdist.dist.type"] = "uniform"
-cfg["hivtransmission.m_condomformationdist.dist.uniform.max"] = 1
-cfg["hivtransmission.m_condomformationdist.dist.uniform.min"] = 0
+cfg["EventCondom.threshold"] = 0.5      # threshold for condom preference for an individual 
+cfg["hivtransmission.threshold"] = 0.5  # threshold for condom use in formation
+cfg["hivtransmission.m_condomformationdist.dist.type"] = "discrete.csv.twocol"
+cfg["hivtransmission.m_condomformationdist.dist.discrete.csv.twocol.file"] = "/home/jupyter/who-aids-prop/build/python/relationship_condom_use.csv"
+cfg["hivtransmission.m_condomformationdist.dist.discrete.csv.twocol.floor"] = 'yes' #to force distirbution to return exact values and not any value within the bin
 
 # cfg["EventVMMC.m_vmmcprobDist.dist.type"] = "uniform"
 # cfg["EventVMMC.m_vmmcprobDist.dist.uniform.max"] = 1
@@ -48,7 +48,7 @@ cfg["hivtransmission.m_condomformationdist.dist.uniform.min"] = 0
 # cfg["EventVMMC.m_vmmcscheduleDist.dist.discrete.csv.twocol.file"] = "/home/jupyter/who-aids-prop/build/python/vmmc_schedule_twocol_1.csv"
 
 res = simpact.run(cfg, "/home/jupyter/who-aids-prop/build/python/output",
-                  seed=1, 
+                  seed=42, 
                   interventionConfig=[iv1],
                   dataFiles=data)
 
