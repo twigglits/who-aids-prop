7dbd39b5288118d2c8bec98c05df509a16296795
lilslugger <jnaude60512@gmail.com>
2024-06-18 15:14:58 +0200

    updated eventcondom removed section where we reset the condom ref

--- src/program-common/eventcondom.cpp
+++ src/program-common/eventcondom.cpp
@@ -44,15 +44,13 @@ bool EventCondom::isEligibleForTreatment(double t, const State *pState)
     double age = pPerson->getAgeAt(curTime); 
     cout << "Checking eligibility for person " << pPerson->getName() << " with age: " << age << endl;
     
-    if (pPerson->isSexuallyActive() && pPerson->getNumberOfRelationships() > 0) {
+    if (pPerson->isSexuallyActive()) {
         cout << "Person " << pPerson->getName() << " Condom eligible with age: " << age << endl;
         return true;  // eligible for condom programming
-    }else if (pPerson->getNumberOfRelationships() == 0)
-    {
+    }else {        
         cout << "Person " << pPerson->getName() << " Condom NOT eligible with age: " << age << endl;
         return false;
     }
-    return false;
 }
 
 bool EventCondom::isWillingToStartTreatment(double t, GslRandomNumberGenerator *pRndGen) {
